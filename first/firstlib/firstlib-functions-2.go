1f5eb387a3b488a69b7b92875455b2b1f7fca2cf
Raj Chaudhuri <rajch@hotmail.com>
2017-07-28 23:36:31 +0530

    Hello, defer

--- first/firstlib/firstlib-functions-2.go
+++ first/firstlib/firstlib-functions-2.go
@@ -2,14 +2,16 @@ package firstlib
 
 var cnt int = 0
 
+func increment() {
+	cnt++
+}
 
 func Init() {
 	cnt = 0;
 }
 
 func Next() (int) {
-	result := cnt
-	cnt++
-	return result
+	defer increment()
+	return cnt
 }
  
