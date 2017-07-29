cdc7ee9149fd4b1d89fdb63f055371a5b031f712
Raj Chaudhuri <rajch@hotmail.com>
2017-07-29 10:21:52 +0530

    Hello, anonymous functions and function passing.

--- matrixlib/matrixlib.go
+++ matrixlib/matrixlib.go
@@ -1,25 +1,49 @@
 package matrixlib
 
-func Create(size int)([][]int) {
+// Create creates a square matrix of the specified dimension
+func Create(size int) [][]int {
 	result := make([][]int, size)
-	for i:= range result {
+	for i := range result {
 		result[i] = make([]int, size)
 	}
 	return result
 }
 
-func Add(one [][]int, two [][]int) ([][]int, bool) {
-	var resultval [][]int
-	result := len(one) == len(two) && len(one[0]) == len(two[0])
-	if result {
-		resultval = Create(len(one))
+func rowwiseoperate(one [][]int, two [][]int, operation func(int, int) int) ([][]int, bool) {
+	var resultmatrix [][]int
+
+	resultstatus := len(one) == len(two)
+
+	if resultstatus {
+		resultmatrix = Create(len(one))
 		for i, onerow := range one {
-			for j, oneval := range onerow {
-				resultval[i][j] = oneval + two[i][j]
+			if len(onerow) == len(two[i]) {
+				for j, oneval := range onerow {
+					resultmatrix[i][j] = operation(oneval, two[i][j])
+				}
+			} else {
+				resultstatus = false
+				resultmatrix = nil
+				break
 			}
 		}
-	} else {
-		resultval = nil
 	}
-	return resultval, result
+
+	return resultmatrix, resultstatus
+}
+
+// Add adds two matrices
+func Add(one [][]int, two [][]int) ([][]int, bool) {
+	resultmatrix, resultstatus := rowwiseoperate(one, two, func(leftval int, rightval int) int {
+		return leftval + rightval
+	})
+	return resultmatrix, resultstatus
+}
+
+// Subtract subtracts two matrices
+func Subtract(one [][]int, two [][]int) ([][]int, bool) {
+	resultmatrix, resultstatus := rowwiseoperate(one, two, func(leftval int, rightval int) int {
+		return leftval - rightval
+	})
+	return resultmatrix, resultstatus
 }
--- matrixmain/matrixmain.go
+++ matrixmain/matrixmain.go
@@ -4,16 +4,56 @@ import "fmt"
 import "github.com/rajch/learngo/matrixlib"
 
 func main() {
+	// Case 1: Correct
+	fmt.Println("Case 1: Add m1(3x3) to m2(3x3)")
 	m1 := matrixlib.Create(3)
 	m1[0][0] = 10
 	m1[1][1] = 20
 	m1[2][2] = 30
-	fmt.Println(m1)
-	m2 := matrixlib.Create(4)
+
+	m2 := matrixlib.Create(3)
 	m2[0][0] = 40
 	m2[1][1] = 50
 	m2[2][2] = 60
+
+	m3, ok := matrixlib.Add(m1, m2)
+
+	fmt.Println(m1)
 	fmt.Println(m2)
-	m3, ok := matrixlib.Add(m1,m2)
 	fmt.Println(m3, ok)
+
+	// Case 2: Wrong
+	fmt.Println("\nCase 2: Add m1(3x3) to m4(4x4)")
+	m4 := matrixlib.Create(4)
+	m4[0][0] = 40
+	m4[1][1] = 50
+	m4[2][2] = 60
+
+	m3, ok = matrixlib.Add(m1, m4)
+
+	fmt.Println(m1)
+	fmt.Println(m4)
+	fmt.Println(m3, ok)
+
+	// Case 3: Correct
+	fmt.Println("\nCase 3: Subtract m1(3x3) from m2(3x3)")
+	m3, ok = matrixlib.Subtract(m2, m1)
+
+	fmt.Println(m2)
+	fmt.Println(m1)
+	fmt.Println(m3, ok)
+
+	// Case 4: Wrong
+	fmt.Println("\nCase 4: Add m1(3x3) to m5(jagged)")
+	m5 := matrixlib.Create(3)
+	m5[0][0] = 40
+	m5[1][1] = 50
+	m5[2][2] = 60
+	m5[1] = append(m5[1], 75)
+
+	m3, ok = matrixlib.Add(m1, m5)
+	fmt.Println(m1)
+	fmt.Println(m5)
+	fmt.Println(m3, ok)
+
 }
