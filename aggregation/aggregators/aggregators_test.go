cee0f2b0c8ee8bb1c7d675c1010d00f55bed1b64
Raj Chaudhuri <rajch@hotmail.com>
2017-07-30 21:00:04 +0530

    Library made. Test written. Code not written. Fail.

--- aggregation/aggregators/aggregators_test.go
+++ aggregation/aggregators/aggregators_test.go
@@ -1,9 +1,13 @@
 package aggregators
 
 import "testing"
+import "github.com/rajch/learngo/aggregation/aggregators/intaggregators"
 
 func TestSimpleAggregate(t *testing.T) {
-	t.Error("Test not written")
+	total, cnt := SimpleAggregate(intaggregators.IntSumAggregator{}, []int{1, 2, 3, 4, 5})
+	if total != 15 || cnt != 5 {
+		t.Error("Total not calculated properly")
+	}
 }
 
 func TestPartionedAggregate(t *testing.T) {
