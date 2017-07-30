8634310f3f19838ebde7c725c9f9c2dc8fbb7966
Raj Chaudhuri <rajch@hotmail.com>
2017-07-30 22:12:31 +0530

    Test also written. Show coverage final time.

--- aggregation/aggregators/aggregators_test.go
+++ aggregation/aggregators/aggregators_test.go
@@ -5,9 +5,16 @@ import "github.com/rajch/learngo/aggregation/aggregators/intaggregators"
 
 func TestSimpleAggregate(t *testing.T) {
 	ag := new(intaggregators.IntSumAggregator)
+	avgag := new(intaggregators.IntAvgAggregator)
+
 	total, cnt := SimpleAggregate(ag, []int{1, 2, 3, 4, 5})
 	if total != 15 || cnt != 5 {
 		t.Errorf("Total not calculated properly. Total was %v, count %v.", total, cnt)
+	}
+
+	avg, cnt := SimpleAggregate(avgag, []int{1, 2, 3, 4, 5})
+	if avg != 3 || cnt != 5 {
+		t.Errorf("Average not calculated properly. Average was %v, count %v.", avg, cnt)
 	}
 }
 
